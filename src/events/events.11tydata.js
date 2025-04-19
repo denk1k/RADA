5ea99c371f71df40f63d72c1a8796b4d96275999
flopius <flopodude@gmail.com>
2025-04-19 15:12:47 +0300

    fxed

--- src/events/events.11tydata.js
+++ src/events/events.11tydata.js
@@ -3,12 +3,12 @@ export default function() {
     layout: "layout-event.njk", // Default layout for event pages
     tags: "event", // Tag for the Eleventy collection
     eleventyComputed: {
-      // Dynamically generate the permalink based on the event title
+      // Dynamically generate the permalink based on the event file slug
       permalink: function(data) {
-        // Use the 'title' from front matter
-        const slug = data.title;
-        // Return the permalink like /events/event-title/
-        return `/events/${this.slugify(slug)}/index.html`;
+        // Use the fileSlug (derived from filename) for uniqueness
+        const slug = data.page.fileSlug;
+        // Return the permalink like /events/file-slug/
+        return `/events/${slug}/index.html`;
       }
     }
   };
